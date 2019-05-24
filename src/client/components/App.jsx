113cdacb784a464b13a4fde413002061fdd9019e
Chad Cramer <chcr6493@colorado.edu>
2019-05-24 16:34:19 -0500

    process

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Recommendation from './Recommendation';
+import Recommendation from './Recommendation.jsx';
 
 class App extends React.Component {
     constructor(props) {
--- src/client/components/Restaurant.jsx
+++ src/client/components/Restaurant.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Carousel from './Carousel';
-import Description from './Description';
+import Carousel from './Carousel.jsx';
+import Description from './Description.jsx';
 
 class Restaurant extends React.Component {
     constructor(props) {
