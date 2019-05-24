9ce0cbc5eba51530ecb67b46c39e70ad89f8612a
Chad Cramer <chcr6493@colorado.edu>
2019-05-24 18:23:37 -0500

    3rd time

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-      name: 'Chad-Service',
+      name: 'Chad',
       script: './server/server.js'
     }],
     deploy: {
@@ -10,7 +10,7 @@ module.exports = {
         key: '~/.ssh/fec.pem',
         ref: 'origin/master',
         repo: 'https://github.com/yum-ly/Chad-Service.git',
-        path: '/home/ubuntu/Chad-Service',
+        path: '/home/ubuntu/service',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }
