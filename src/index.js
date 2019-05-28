a92aa85e9d735c850ef58d9a9b5c05a87c18b718
Chad Cramer <chcr6493@colorado.edu>
2019-05-28 12:31:48 -0500

    css taking priority over bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './client/components/App.jsx';
-import '../src/style.css';
+
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../src/style.css';
 
 ReactDOM.render(< App />, document.getElementById('root'));
