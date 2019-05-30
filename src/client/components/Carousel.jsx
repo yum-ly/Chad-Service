f95fc4f5510fc3d0c22452987bab4955e9ba342b
Chad Cramer <chcr6493@colorado.edu>
2019-05-30 17:37:41 -0500

    fd

--- src/client/components/Carousel.jsx
+++ src/client/components/Carousel.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import Slider from "react-slick";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "black", color: 'white' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "black", color: 'white' }}
+      onClick={onClick}
+    />
+  );
+}
+
 class Carousel extends React.Component {
+  
 
   render(props) {
     const settings = {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
     };
 
     return (
