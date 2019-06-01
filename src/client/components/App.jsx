89b8bd6438f7b6cc62e30433297d75ea694242e0
Chad Cramer <chcr6493@colorado.edu>
2019-06-01 11:50:28 -0500

    safd

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Recommendation from './Recommendation.jsx';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYenSign, faStar } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core';
 
