3510167a04d43dfd87f2c059aab0de2cadae220b
Chad Cramer <chcr6493@colorado.edu>
2019-07-09 17:04:09 -0500

    ecosystem.config file update for readability

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,18 @@ module.exports = {
     }],
     deploy: {
       production: {
+        // user used to authenticate
         user: 'ubuntu',
+        // Where to connect
         host: 'ec2-52-14-174-92.us-east-2.compute.amazonaws.com',
+        // Path to the private key to authenticate
         key: '~/.ssh/fec.pem',
+        // The Git branch required
         ref: 'origin/master',
+        // The path to the source code repository
         repo: 'https://github.com/yum-ly/Chad-Service.git',
+        // 'path' is path on the host server where you want to deploy the source code 
+        // 'post-deploy' allows you to specify a command to run after the deployment is complete
         path: '/home/ubuntu/service',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
